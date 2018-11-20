c4512c49d67aba949d0232a6d14f5134281e14bb
liupan <kaho.liu@yahoo.com>
2018-11-20 17:33:56 +0800

    add supervisor support

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 rsyslogd
 /etc/init.d/cron start
-/etc/init.d/cron reload
 touch /var/spool/cron/crontabs/root
 echo  "* * * * * /usr/local/bin/php /opt/code/${PROJECT_NAME}/artisan schedule:run" >> /var/spool/cron/crontabs/root
+/etc/init.d/cron reload
+service supervisor starts
+supervisorctl reload
 docker-php-entrypoint php-fpm
